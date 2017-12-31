850be0afe34fb7d693bb2fb53e4ee5c86163517f
Carlos De la Mora <carlosdelamorac@gmail.com>
2017-12-31 12:49:15 -0700

    feat: get json for cid

--- ARMolecule/PublicChemClient.swift
+++ ARMolecule/PublicChemClient.swift
@@ -7,21 +7,77 @@
 //
 
 import Foundation
-
+import UIKit
 
 class PublicChemClient{
     
     private var cidDataTask: URLSessionDataTask? = nil
     private(set) var state: State = .notSearchedYet
     typealias  SearchComplete = (Bool) -> Void
-    
+    typealias haveCID = (Int?) -> Void
     enum State{
-        case notSearchedYet //this is the state in case of an error
+        case notSearchedYet
         case loading
         case noResults
         case results([Molecule])
     }
     
+    func performCIDSearch(searchText: String, haveCID:@escaping haveCID){
+        if !searchText.isEmpty{
+            cidDataTask?.cancel()
+            state = .loading
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            let url = cidURL(name: searchText)
+            let session = URLSession.shared
+            cidDataTask = session.dataTask(with: url, completionHandler: { (data,response, error) in
+                
+                var cid:Int? = nil
+                
+                if let error = error as NSError?, error.code == -999 {
+                    return
+                }else if let response = response as? HTTPURLResponse, 200 <= response.statusCode && response.statusCode <= 299{
+                    
+                    if let data = data, let jsonDictionary = self.parseJsonForCID(json: data){
+                        
+                        print(jsonDictionary)
+                        /*var searchResults = self.parseDictionary(dictionary: jsonDictionary)
+                        if searchResults.isEmpty {
+                            
+                            self.state = .noResults
+                        }else{
+                            searchResults.sort(by: <)
+                            
+                            self.state = .results(searchResults)
+                        }
+                        success = true*/
+                    }
+                }
+                
+                DispatchQueue.main.async {
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                    haveCID(cid)
+                }
+            })
+            
+            cidDataTask?.resume()
+        }
+    }
     
+    func cidURL(name: String)-> URL {
+        let stringURL = String(format: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/%@/json", arguments: [name])
+        let url = URL(string:stringURL)
+        return url!
+    }
+    
+    private func parseJsonForCID(json data: Data)-> [String: Any]?{
+        
+        do{
+            return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        }catch{
+            print("Json Error \(error)")
+            return nil
+        }
+    }
     
 }
--- ARMolecule/ViewControllers/SearchViewController.swift
+++ ARMolecule/ViewControllers/SearchViewController.swift
@@ -10,17 +10,40 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var moleculeName: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    let client = PublicChemClient()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    
 }
 
+extension SearchViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        client.performCIDSearch(searchText: searchBar.text!) { (cid) in
+            print(cid)
+        }
+    }
+    
+    
+    
+}
