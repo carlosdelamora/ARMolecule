0ef1dc796b9f7ffb90f3b52b201fec2608cd09cc
Carlos De la Mora <carlosdelamorac@gmail.com>
2018-01-05 22:31:32 -0700

    feat: add molecule at different locations

--- ARMolecule/ViewControllers/SceneViewController.swift
+++ ARMolecule/ViewControllers/SceneViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import SceneKit
 import ARKit
+import simd
+import GLKit
 
 class SceneViewController: UIViewController, ARSCNViewDelegate {
 
@@ -22,7 +24,13 @@ class SceneViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(placeMolecule))
+        /*
+         Prevent the screen from being dimmed after a while as users will likely
+         have long periods of interaction without touching the screen or buttons.
+         */
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action:#selector(addAnAchor))
         sceneView.addGestureRecognizer(tapGesture)
         // Create a new scene
         //let scene = SCNScene(named: "art.scnassets/ship.scn")!
@@ -48,33 +56,39 @@ class SceneViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-  
+    
+    
     @objc
-    func placeMolecule(){
-        
-        guard let molecule = molecules.first else{
-            return
-        }
-        
+    func addAnAchor(){
         guard let currentFrame = sceneView.session.currentFrame else{
             return
         }
-        
         var translation = matrix_identity_float4x4
         translation.columns.3.z = -0.30
-        
+        let rotation = float4x4.makeRotate(radians: -.pi/2, 0, 0, 1)
+        let cameraTransform = currentFrame.camera.transform
+        let firstProduct = matrix_multiply(translation,rotation)
+        let product = matrix_multiply(cameraTransform, firstProduct)
+        let anchor = ARAnchor(transform: product)
+        sceneView.session.add(anchor: anchor)
+    }
+  
+    
+    func placeMolecule(node: SCNNode){
+        guard let molecule = molecules.first else{
+            return
+        }
         //let normalizedConformers = molecule.normalizedConformers
         let moleculeNode = molecule.createSceneMolecule()
-        //moleculeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
-        //sceneView.debugOptions = ARSCNDebugOptions.showWorldOrigin
         moleculeNode.position = SCNVector3(0,0, -molecule.radiusOfTheMolecule - 0.40)
-        sceneView.scene.rootNode.addChildNode(moleculeNode)
-        
+        node.addChildNode(moleculeNode)
     }
     
     
-
-    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        placeMolecule(node:node)
+    }
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -99,5 +113,13 @@ class SceneViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+}
+
+
+extension float4x4 {
+  
+    static func makeRotate(radians: Float, _ x: Float, _ y: Float, _ z: Float) -> float4x4 {
+        return unsafeBitCast(GLKMatrix4MakeRotation(radians, x, y, z), to: float4x4.self)
     }
 }
